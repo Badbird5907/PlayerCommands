f039da0a9d9d70b7567bcaf76fd6982aca05d559
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-01-10 13:41:39 -0500

    Initial commit

---
+++ src/main/java/dev/badbird/playercommands/PlayerCommands.java
@@ -0,0 +1,17 @@
+package dev.badbird.playercommands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerCommands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
