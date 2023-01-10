0100e970d725cb5db97332bf65348ac70502aca0
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-01-10 13:50:10 -0500

    done

--- src/main/java/dev/badbird/playercommands/PlayerCommands.java
+++ src/main/java/dev/badbird/playercommands/PlayerCommands.java
@@ -1,17 +1,69 @@
 package dev.badbird.playercommands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PlayerCommands extends JavaPlugin {
+import java.util.List;
+
+public final class PlayerCommands extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdirs();
+        }
+        saveDefaultConfig();
 
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        List<String> commands = getConfig().getStringList("commands.join");
+        int delay = getConfig().getInt("command.delay");
+        if (delay > 0) {
+            getServer().getScheduler().runTaskLater(this, () -> {
+                execute(commands, event.getPlayer());
+            }, delay);
+        } else {
+            execute(commands, event.getPlayer());
+        }
+    }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event) {
+        List<String> commands = getConfig().getStringList("commands.leave");
+        int delay = getConfig().getInt("command.delay");
+        if (delay > 0) {
+            getServer().getScheduler().runTaskLater(this, () -> {
+                execute(commands, event.getPlayer());
+            }, delay);
+        } else {
+            execute(commands, event.getPlayer());
+        }
+    }
+
+    public void execute(List<String> commands, Player player) {
+        for (String command : commands) {
+            command = command.replace("%player%", player.getName());
+            boolean console = !command.startsWith("player:");
+            if (!console) {
+                command = command.substring(7);
+            }
+            if (command.startsWith("console:")) {
+                console = true;
+                command = command.substring(8);
+            }
+            if (console) {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+            } else {
+                player.performCommand(command);
+            }
+        }
     }
 }
